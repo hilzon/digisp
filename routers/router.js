f4de59092cf238ecdd2d68546b047a80995febb2
hilzon <hilzonm@gmail.com>
2019-12-08 20:12:30 +0700

    updat

--- routers/router.js
+++ routers/router.js
@@ -9,6 +9,8 @@ const newsController = require('../controllers/newsController');
 const kontakController = require('../controllers/kontakController');
 const userController = require('../controllers/userController');
 const adminController = require('../controllers/adminController');
+const chatingController = require('../models/chatingController');
+const chatingDetailController = require('../models/chatingDetailController')
 // const middleware = require('../middlewares/middleware');
 // const verifyToken = require('../middlewares/auth');
 
@@ -82,13 +84,20 @@ module.exports = app => {
   
 
 
+//API Chating
+app.get('/api/chating', chatingController.listchating)
+app.post('/api/chating', chatingController.tambahchating)
+app.put('/api/chating/:id', chatingController.ubahchating)
+app.delete('/api/chating/:id', chatingController.hapuschating)
+
+//API ChatingDetail
+app.get('/api/chatingDetail', chatingDetailController.listchatingDetail)
+app.post('/api/chatingDetail', chatingDetailController.tambahchatingDetail)
+app.put('/api/chatingDetail/:id', chatingDetailController.ubahchatingDetail)
+app.delete('/api/chatingDetail/:id', chatingDetailController.hapuschatingDetail)
 
 
 
-// //API adminLogin
-// app.post('/api/register', middleware.postRegister)
-// app.get('/api/admin', adminController.listAdmin)
-   
 
 };
  
